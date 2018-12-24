594225cf37a46cac259f6bbc60e2c7690eff2db2
zhaizhaizhai <269351523@qq.com>
2018-12-24 21:33:11 +0800

    实现公众号天气功能

--- wx.php
+++ wx.php
@@ -1,107 +1,159 @@
 <?php
-    //获得参数 signature nonce token timestamp echostr
-    $nonce     = $_GET['nonce'];
-    $token     = 'imooc';
-    $timestamp = $_GET['timestamp'];
-    $echostr   = $_GET['echostr'];
-    $signature = $_GET['signature'];
-    //形成数组，然后按字典序排序
-    $array = array();
-    $array = array($nonce, $timestamp, $token);
-    sort($array);
-    //拼接成字符串,sha1加密 ，然后与signature进行校验
-    $str = sha1( implode( $array ) );
-    if( $str == $signature && $echostr ){
-        //第一次接入weixin api接口的时候
-        echo  $echostr;
-        exit;
-    }
-    else{
+/**
+  * wechat php test
+  */
 
-        //1.获取到微信推送过来post数据（xml格式）
-        $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
-        //2.处理消息类型，并设置回复类型和内容
-        /*<xml>
-         <ToUserName><![CDATA[toUser]]></ToUserName>
-         <FromUserName><![CDATA[FromUser]]></FromUserName>
-         <CreateTime>123456789</CreateTime>
-         <MsgType><![CDATA[event]]></MsgType>
-         <Event><![CDATA[subscribe]]></Event>
-          </xml>*/
-        $postObj = simplexml_load_string( $postArr );
-        //$postObj->ToUserName = '';
-        //$postObj->FromUserName = '';
-        //$postObj->CreateTime = '';
-        //$postObj->MsgType = '';
-        //$postObj->Event = '';
-        // gh_e79a177814ed
-        //判断该数据包是否是订阅的事件推送
-        if( strtolower( $postObj->MsgType) == 'event'){
-            //如果是关注 subscribe 事件
-            if( strtolower($postObj->Event == 'subscribe') ){
-                //回复用户消息(纯文本格式)
-                $toUser   = $postObj->FromUserName;
-                $fromUser = $postObj->ToUserName;
-                $time     = time();
-                $msgType  =  'text';
-                $content  = '欢迎关注我们的微信公众账号'.$postObj->FromUserName.'-'.$postObj->ToUserName;
-                $template = "<xml>
-                                <ToUserName><![CDATA[%s]]></ToUserName>
-                                <FromUserName><![CDATA[%s]]></FromUserName>
-                                <CreateTime>%s</CreateTime>
-                                <MsgType><![CDATA[%s]]></MsgType>
-                                <Content><![CDATA[%s]]></Content>
-                                </xml>";
-                $info     = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
-                echo $info;
-                /*<xml>
-                <ToUserName><![CDATA[toUser]]></ToUserName>
-                <FromUserName><![CDATA[fromUser]]></FromUserName>
-                <CreateTime>12345678</CreateTime>
-                <MsgType><![CDATA[text]]></MsgType>
-                <Content><![CDATA[你好]]></Content>
-                </xml>*/
+//define your token
+define("TOKEN", "libingzhai");
+$wechatObj = new wechatCallbackapiTest();
+$wechatObj->responseMsg();
+//$wechatObj->valid();
 
+class wechatCallbackapiTest
+{
+    /*public function valid()
+    {
+        $echoStr = $_GET["echostr"];
 
-            }
+        //valid signature , option
+        if($this->checkSignature()){
+            echo $echoStr;
+            exit;
         }
-      //判断是否文本消息
-      if(strtolower( $postObj->MsgType) == 'text'){
-        //接受消息
-        $content = $postObj->Content;
-          //回复用户文字消息
-            $toUser = $postObj->FromUserName;
-            $fromUser = $postObj->ToUserName;
-            $time = time();
-            $msgType = 'text';
-            $content = '北京天气:
-今日温度-1-10℃
-实时温度:8℃ 
-风向风速: 西南风3级
-空气质量: 94 良 
-<a href="http://188.131.202.95/wxWeather.html">点击进入北京天气网页版</a>';      
-           
-        
-          $template = "<xml>
-                           <ToUserName><![CDATA[%s]]></ToUserName>
-                           <FromUserName><![CDATA[%s]]></FromUserName>
-                           <CreateTime>%s</CreateTime>
-                           <MsgType><![CDATA[%s]]></MsgType>
-                           <Content><![CDATA[%s]]></Content>
-                           </xml>";
-              $info = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
-              echo $info;
-             /*<xml>
-              <ToUserName><![CDATA[toUser]]></ToUserName>
-              <FromUserName><![CDATA[fromUser]]></FromUserName>
-              <CreateTime>12345678</CreateTime>
-              <MsgType><![CDATA[text]]></MsgType>
-              <Content><![CDATA[你好]]></Content>
-              </xml>*/
-      
-      }
+    }*/
+
+    public function responseMsg()
+    {
+        //get post data, May be due to the different environments
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+          //extract post data
+        if (!empty($postStr)){
+                
+                  $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $RX_TYPE = trim($postObj->MsgType);
+
+                switch($RX_TYPE)
+                {
+                    case "text":
+                        $resultStr = $this->handleText($postObj);
+                        break;
+                    case "event":
+                        $resultStr = $this->handleEvent($postObj);
+                        break;
+                    default:
+                        $resultStr = "Unknow msg type: ".$RX_TYPE;
+                        break;
+                }
+                echo $resultStr;
+        }else {
+            echo "";
+            exit;
+        }
     }
+
+    public function handleText($postObj)
+    {
+        $fromUsername = $postObj->FromUserName;
+        $toUsername = $postObj->ToUserName;
+        $keyword = trim($postObj->Content);
+        $time = time();
+        $textTpl = "<xml>
+                    <ToUserName><![CDATA[%s]]></ToUserName>
+                    <FromUserName><![CDATA[%s]]></FromUserName>
+                    <CreateTime>%s</CreateTime>
+                    <MsgType><![CDATA[%s]]></MsgType>
+                    <Content><![CDATA[%s]]></Content>
+                    <FuncFlag>0</FuncFlag>
+                    </xml>";             
+        if(!empty( $keyword ))
+        {
+            $msgType = "text";
+
+            //天气
+            $str = mb_substr($keyword,-2,2,"UTF-8");
+            $str_key = mb_substr($keyword,0,-2,"UTF-8");
+            if($str == '天气' && !empty($str_key)){
+                $data = $this->weather($str_key);
+                if(empty($data->cityInfo)){
+                    $contentStr = "抱歉，没有查到\"".$str_key."\"的天气信息！";
+                } else {
+                    $contentStr = "【".$data->cityInfo->city."天气预报】\n".$data->time." "."发布"."\n\n实时天气\n".$data->data->forecast[0]->type." ".$data->data->wendu."℃  ".$data->data->forecast[0]->fx.$data->data->forecast[0]->fl."\npm25: ".$data->data->pm25." 湿度: ".$data->data->shidu."\n\n温馨提示：".$data->data->ganmao.
+                                    "\n\n明天\n".$data->data->forecast[1]->week."\n".$data->data->forecast[1]->type." ".$data->data->forecast[1]->low." ".$data->data->forecast[1]->high."\n".$data->data->forecast[1]->fx." ".$data->data->forecast[1]->fl.
+                                    "\n\n后天\n".$data->data->forecast[2]->week."\n".$data->data->forecast[2]->type." ".$data->data->forecast[2]->low." ".$data->data->forecast[2]->high."\n".$data->data->forecast[2]->fx." ".$data->data->forecast[2]->fl;
+                }
+            } else {
+                $contentStr = "感谢您的关注"."\n"."目前平台功能如下："."\n"."查天气，如输入：北京天气"."\n"."更多内容，敬请期待...";
+            }
+            $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+            echo $resultStr;
+        }else{
+            echo "Input something...";
+        }
+    }
+
+    public function handleEvent($object)
+    {
+        $contentStr = "";
+        switch ($object->Event)
+        {
+            case "subscribe":
+                $contentStr = "感谢您的关注"."\n"."目前平台功能如下："."\n"."查天气，如输入：北京天气"."\n"."更多内容，敬请期待...";
+                break;
+            default :
+                $contentStr = "Unknow Event: ".$object->Event;
+                break;
+        }
+        $resultStr = $this->responseText($object, $contentStr);
+        return $resultStr;
+    }
+    
+    public function responseText($object, $content, $flag=0)
+    {
+        $textTpl = "<xml>
+                    <ToUserName><![CDATA[%s]]></ToUserName>
+                    <FromUserName><![CDATA[%s]]></FromUserName>
+                    <CreateTime>%s</CreateTime>
+                    <MsgType><![CDATA[text]]></MsgType>
+                    <Content><![CDATA[%s]]></Content>
+                    <FuncFlag>%d</FuncFlag>
+                    </xml>";
+        $resultStr = sprintf($textTpl, $object->FromUserName, $object->ToUserName, time(), $content, $flag);
+        return $resultStr;
+    }
+
+    private function weather($n){
+        include("weather_cityId.php");
+        $c_name=$weather_cityId[$n];
+        if(!empty($c_name)){
+            $json=file_get_contents("http://t.weather.sojson.com/api/weather/city/".$c_name);
+            return json_decode($json);
+        } else {
+            return null;
+        }
+    }
+
+    private function checkSignature()
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];    
+                
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
         
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+?>
         
         
       
